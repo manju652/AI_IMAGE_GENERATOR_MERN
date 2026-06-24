a1b35fd95e0f9a304adeede43365ebb877d4864f
manju652 <manjunamana786@gmail.com>
2026-06-24 18:14:16 +0530

    debug post error

--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -1,47 +1,28 @@
-import Post from "../models/Posts.js";
-import * as dotenv from "dotenv";
-import { createError } from "../error.js";
-import { v2 as cloudinary } from "cloudinary";
-
-dotenv.config();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-// Get all posts
-export const getAllPosts = async (req, res, next) => {
-  try {
-    const posts = await Post.find({});
-    return res.status(200).json({ success: true, data: posts });
-  } catch (error) {
-    return next(
-      createError(
-        error.status,
-        error?.response?.data?.error.message || error.message
-      )
-    );
-  }
-};
-
-// Create new post
 export const createPost = async (req, res, next) => {
   try {
     const { name, prompt, photo } = req.body;
+
+    console.log("PHOTO => ", photo);
+
     const photoUrl = await cloudinary.uploader.upload(photo);
+
     const newPost = await Post.create({
       name,
       prompt,
       photo: photoUrl.secure_url,
     });
 
-    return res.status(200).json({ success: true, data: newPost });
+    return res.status(200).json({
+      success: true,
+      data: newPost,
+    });
   } catch (error) {
-    console.log(error);
-    return next(
-      createError(error.status, error?.response?.data?.error.message)
-    );
+    console.log("FULL ERROR =>", error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+      stack: error.stack,
+    });
   }
 };
