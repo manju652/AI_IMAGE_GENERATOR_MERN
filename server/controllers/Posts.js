20558e6fc7755eaf3c625f40627fe8d5ed0c0d94
manju652 <manjunamana786@gmail.com>
2026-06-24 21:47:01 +0530

    Debug cloudinary error

--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -25,13 +25,28 @@ export const getAllPosts = async (req, res, next) => {
 
 export const createPost = async (req, res, next) => {
   try {
-    console.log("BODY:", req.body);
+    const { name, prompt, photo } = req.body;
+    console.log("PHOTO =", photo);
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.secure_url,
+    });
 
     return res.status(200).json({
       success: true,
-      message: "POST route working",
+      data: newPost,
     });
   } catch (error) {
     console.log(error);
+
+    return next(
+      createError(
+        error.status || 500,
+        error?.response?.data?.error?.message || error.message
+      )
+    );
   }
 };
