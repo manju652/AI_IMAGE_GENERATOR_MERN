34be94cfcd38bd6a91b669d0a06c04a6bc78019e
manju652 <manjunamana786@gmail.com>
2026-06-24 16:32:09 +0530

    remove unused lightTheme

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// http://localhost:8080/api/
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://ai-image-generator-mern-ncoc.onrender.com/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
